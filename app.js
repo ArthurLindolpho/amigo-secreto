209ee6281bbb1e49994171830960c1d283b6d0ea
Arthur Lindolpho <133370641+ArthurLindolpho@users.noreply.github.com>
2025-08-12 19:49:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaNomes = []
+let listaSorteados = []
 
 function mostrarNomes() {
     let lista = document.querySelector('#listaAmigos');
@@ -32,5 +33,16 @@ function sortearAmigo() {
         let nomeSorteado = listaNomes[Math.floor(Math.random() * listaNomes.length)]
         let resultado = document.querySelector('#resultado') 
         resultado.innerHTML = `<li>${nomeSorteado}<li>`
+        if (listaSorteados.includes(nomeSorteado)){
+            return sortearAmigo()
+        }else{
+            listaSorteados.push(nomeSorteado)
+        }
+    }
+    if (listaSorteados.length == listaNomes.length){
+        setTimeout(() => {
+            alert('Todos os nomes já foram sorteados!')
+        }, 2000)
+        return
     }
 }
