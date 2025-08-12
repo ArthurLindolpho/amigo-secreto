2223bf8f09871bc0d7b53e5c16e3fd44eb43a39f
Arthur Lindolpho <133370641+ArthurLindolpho@users.noreply.github.com>
2025-08-12 19:19:26 -0300

    Add files via upload
    Este foi o challenge de iniciante em programação do grupo one em parceria com a alura, neste desafio tive que simular um amigo secreto utilizando java script, neste projeto foram utilizados conceitos básicos da lógica de programação em java script e foram utilizados assuntos como listas e funções.

---
+++ app.js
@@ -0,0 +1,36 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = []
+
+function mostrarNomes() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = listaNomes.join(', '); 
+}
+
+function adicionarAmigo(){
+    let nome = document.querySelector('input').value
+    if (nome == ''){
+        alert('Este campo não pode estar vazio!')
+    } else{
+        listaNomes.push(nome)
+        limparCampo()
+        mostrarNomes()
+    }
+
+}
+
+function limparCampo(){
+    nome = document.querySelector('input')
+    nome.value = ''
+}
+
+
+function sortearAmigo() {
+    if (listaNomes.length == 0){
+        alert('Nenhum nome na lista para sortear.')
+        return
+    } else{
+        let nomeSorteado = listaNomes[Math.floor(Math.random() * listaNomes.length)]
+        let resultado = document.querySelector('#resultado') 
+        resultado.innerHTML = `<li>${nomeSorteado}<li>`
+    }
+}
